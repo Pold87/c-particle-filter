abba47f8c4e905ea7ba53e4e28dfba33805b9545
Volker Strobel <volker.strobel87@gmail.com>
2016-02-14 23:50:36 +0100

    Cleaned up and restored makefile

--- conv_func.m
+++
@@ -1,9 +0,0 @@
-%#codegen
-function [ z ] = conv_func( x, y )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-    z = x + y;
-
-end
-
--- conv_to_c.m
+++
@@ -1,3 +0,0 @@
-a = 5;
-b = 3;
-fprintf('The result is %d\n', a + b);
